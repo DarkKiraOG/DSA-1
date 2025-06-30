bcfd81257cc06fa126954c58b61276f12655a5ec
Mohammad Quayim <145481347+DarkKiraOG@users.noreply.github.com>
2025-06-30 23:22:26 +0530

    Update Increase_array_size.cpp
    Added Description.

--- Notes/Increase_array_size.cpp
+++ Notes/Increase_array_size.cpp
@@ -3,7 +3,7 @@
 
 void main(){
 
-  int *p = new int[5];
+  int *p = new int[5]; // Increasing array size is ony possible with Dynamic Memory Allocation.
   int *q = new int[10];
 
   for (int i = 0; i < 5; i++){
@@ -12,6 +12,6 @@ void main(){
 
   delete p;
   p = q;
-  q = NULL;
+  q = NULL; //Don't need two pointers.
   
 }
