e4ce4f4074562fba65d8fa1e8780ceb2b090e53f
Mohammad Quayim <145481347+DarkKiraOG@users.noreply.github.com>
2025-06-30 23:20:40 +0530

    Create Increase_array_size.cpp

---
+++ Notes/Increase_array_size.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<iostream.h>
+
+void main(){
+
+  int *p = new int[5];
+  int *q = new int[10];
+
+  for (int i = 0; i < 5; i++){
+    q[i] = p[i];
+  }
+
+  delete p;
+  p = q;
+  q = NULL;
+  
+}
