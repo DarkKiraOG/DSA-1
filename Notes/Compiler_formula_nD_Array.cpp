46e0349fa1e181f9b7ec403141466e86a629b1ea
Mohammad Quayim <145481347+DarkKiraOG@users.noreply.github.com>
2025-07-03 14:52:17 +0530

    Update Compiler_formula_nD_Array.cpp

--- Notes/Compiler_formula_nD_Array.cpp
+++ Notes/Compiler_formula_nD_Array.cpp
@@ -1,5 +1,14 @@
 int array[a][b][c][d];         //Or any type
 
-// Row Major Formula:            Address(array[i][j][k][l]) = Address(array) + [i*b*c*d + j*c*d + k*d + l] * sizeoftype
+/*
 
-// Column Major Formula:         Address(array[i][j][k][l]) = Address(array) + [l*a*b*c + k*b*c + j*a + i] * sizeoftype
+Row Major Formula:            Address(array[i][j][k][l]) = Address(array) + [i*b*c*d + j*c*d + k*d + l] * sizeoftype
+
+-> Time Complexity of Operations: O(n^2)
+We can make it more efficient by reversing the terms in the bracket and taking out common factors:
+
+-> Address(array) + [l + d*[k + c*[j + b*[i]]]] * sizeoftype
+
+Column Major Formula:         Address(array[i][j][k][l]) = Address(array) + [l*a*b*c + k*b*c + j*a + i] * sizeoftype
+
+*/
