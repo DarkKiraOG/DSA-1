12eb93a51411b50afd0d3d52310f310ed3eeb53b
Mohammad Quayim <145481347+DarkKiraOG@users.noreply.github.com>
2025-07-03 14:53:34 +0530

    Update Compiler_formula_nD_Array.cpp

--- Notes/Compiler_formula_nD_Array.cpp
+++ Notes/Compiler_formula_nD_Array.cpp
@@ -5,7 +5,7 @@ int array[a][b][c][d];         //Or any type
 Row Major Formula:            Address(array[i][j][k][l]) = Address(array) + [i*b*c*d + j*c*d + k*d + l] * sizeoftype
 
 -> Time Complexity of Operations: O(n^2)
-We can make it more efficient by reversing the terms in the bracket and taking out common factors:
+We can make it more efficient by reversing the terms in the bracket and taking out common factors: (Horner's Rule)
 
 -> Address(array) + [l + d*[k + c*[j + b*[i]]]] * sizeoftype
 Now: Time Complexity of Operations: O(n)
