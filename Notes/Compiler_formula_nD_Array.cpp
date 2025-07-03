649fe956d0e454338e7c8f14f7c3f719c2d5e36c
Mohammad Quayim <145481347+DarkKiraOG@users.noreply.github.com>
2025-07-03 14:42:13 +0530

    Update Compiler_formula_nD_Array.cpp

--- Notes/Compiler_formula_nD_Array.cpp
+++ Notes/Compiler_formula_nD_Array.cpp
@@ -1,3 +1,5 @@
 int array[a][b][c][d];         //Or any type
 
-//  Address(array[i][j][k][l]) = Address(array) + [i*b*c*d + j*c*d + k*d + l] * sizeoftype
+// Row Major Formula:            Address(array[i][j][k][l]) = Address(array) + [i*b*c*d + j*c*d + k*d + l] * sizeoftype
+
+// Column Major Formula:         Address(array[i][j][k][l]) = Address(array) + [l*a*b*c + k*b*c + j*a + i] * sizeoftype
