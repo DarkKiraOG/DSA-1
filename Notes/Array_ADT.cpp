dbe7a3c46e19df6e10373fb493843b4d5100a56f
Mohammad Quayim <145481347+DarkKiraOG@users.noreply.github.com>
2025-07-28 12:39:19 +0530

    Update Array_ADT.cpp

--- Notes/Array_ADT.cpp
+++ Notes/Array_ADT.cpp
@@ -22,3 +22,22 @@ int main()
   cin>> &arr.A[i];
   }
   arr.length = n;
+}
+
+// Append
+
+void Append(struct Array *arr, int x){
+    if (arr -> length < arr -> size)
+        arr -> A[arr -> length++] = x;
+}
+
+// Insert 
+
+void Insert(struct Array *arr, int n, int x){
+    if (arr -> length < arr -> size)
+        int holder;
+        for (int i = n; i < A -> size; i++)
+            holder = arr -> A[i];
+            arr -> A[i] = x;
+            x = holder
+}
