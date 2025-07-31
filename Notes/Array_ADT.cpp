b87a2f0ca29b125bfe90e9b2d7d19e6fc4e52955
Mohammad Quayim <145481347+DarkKiraOG@users.noreply.github.com>
2025-07-31 14:40:41 +0530

    Update Array_ADT.cpp

--- Notes/Array_ADT.cpp
+++ Notes/Array_ADT.cpp
@@ -57,25 +57,61 @@ void Insert(struct Array *arr, int index, int x){
 }
 
 
-// Delete     //self made
+// Delete     
 
-void Delete(struct Array *arr. int index) {
+int Delete(struct Array *arr. int index) {
+    int x = 0;
     if (index >=0 && index <= arr -> length){
+        x = arr->A[index];
         for (int i = index; i < (arr -> length) - 1; i++)
             {
                 arr -> A[i] = arr -> A[i+1];
             }
         arr -> length --;
-    
+        return x;
     }
+}
     
+//Linear Search
+
+
+    
+for( int i = 0; i < arr -> length ; i++ )
+	 	 {
+	 	 	 if( key == arr->A[ i ] )
+	 	 	 return i;	 	 	 //if search is successful it ends here
+	 	 }
+	 	 
+
+	 	 return -1;	 	 	 	 // if search unsuccessful returns -1
+
+// Improving Linear Search
+
+//To improve the speed of comparison , you can move a key
+//element repeatedly search one step forward this method is called
+//transposition
+
+for( i = 0; i < length ; i++ )
+{
+	 	 if( key == A[ i ] )
+	 	 {
+	 	 	 swap( A[i], A[i-1]);
+	 	 	 return i-1;
+	 	 }
 }
 
 
+//The second method is you can directly swap the key element to
+//the first element this process is called move to head . The next
+//search for the same element becomes faster.
 
+for( i = 0; i < length ; i++ )
+{
+	 	 if( key == A[ i ] )
+	 	 {
+	 	 	 swap( A[i], A[0]);
+	 	 	 return 0;
+	 	 }
 }
-
-
-
 
 
